ca7030d2b764495c9e4d5eec4bc8f4c74e344ab7
eliath <eliasmartinezcohen@gmail.com>
2014-06-23 19:49:23 -0400

    CV

--- www/js/main.js
+++ www/js/main.js
@@ -4,20 +4,31 @@ controller = {
 		$('body').animate({'opacity': 1}, 1500);
 		this.$el = $('#main');
 		this.$contact = $('#contact');
+		window.history.replaceState({page: 'home'}, '');
+		window.onpopstate = this.popHandler;
 		console.log('Ready');
 	},
 
-	//TODO: psuh/popstate: https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
+	//TODO: psuh/popstate: 
 	toggleContact: function toggleContact() {
 		$('#contact_button').toggleClass('active');
 		var $el = this.$contact;
-		if ($el.is(':visible')) {
+		if ($el.is(':visible')) { //Hide
 			$el.animate({'opacity': 0}, 1000, function() {
 				$el.css('display', 'none');
+				window.history.replaceState({page: 'home'}, 'Raphael Cohen', '/');
 			});
-		} else {
+		} else { //Show
 			$el.css('display', 'block');
+			window.history.replaceState({page: 'contact'}, 'Contact | Raphael Cohen', 'contact');
 			$el.animate({'opacity': 1}, 1000);
+		}
+	},
+
+	popHandler: function popHandler(event) {
+		if (event.state.page === 'home') {
+			event.preventDefault();
+			controller.toggleContact();
 		}
 	},
 
