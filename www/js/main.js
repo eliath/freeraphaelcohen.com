be2d4bcd80638c45a957f86d27737e7bd69e3fa9
eliath <eliasmartinezcohen@gmail.com>
2014-06-23 14:44:02 -0400

    Workspace

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,7 @@
 controller = {
 
 	init: function init() {
+		$('body').animate({'opacity': 1}, 1500);
 		this.$el = $('#main');
 		this.$contact = $('#contact');
 		console.log('Ready');
@@ -8,17 +9,15 @@ controller = {
 
 	//TODO: psuh/popstate: https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
 	toggleContact: function toggleContact() {
+		$('#contact_button').toggleClass('active');
 		var $el = this.$contact;
 		if ($el.is(':visible')) {
 			$el.animate({'opacity': 0}, 1000, function() {
-				$el.css({'display': 'none', 'opacity': 0});
-				$('#contact_button').toggleClass('active');
+				$el.css('display', 'none');
 			});
 		} else {
 			$el.css('display', 'block');
-			$el.animate({'opacity': 1}, 1000, function() {
-				$('#contact_button').toggleClass('active');
-			});
+			$el.animate({'opacity': 1}, 1000);
 		}
 	},
 
