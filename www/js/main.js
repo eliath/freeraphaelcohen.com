549ec3656abd732242233bd182c7834d47ed9222
eliath <eliasmartinezcohen@gmail.com>
2014-06-23 20:43:11 -0400

    Ready for deploy

--- www/js/main.js
+++ www/js/main.js
@@ -4,31 +4,43 @@ controller = {
 		$('body').animate({'opacity': 1}, 1500);
 		this.$el = $('#main');
 		this.$contact = $('#contact');
-		window.history.replaceState({page: 'home'}, '');
+		window.history.pushState({page: 'home'}, '');
 		window.onpopstate = this.popHandler;
 		console.log('Ready');
 	},
 
 	//TODO: psuh/popstate: 
-	toggleContact: function toggleContact() {
-		$('#contact_button').toggleClass('active');
+	toggleContact: function toggleContact(x) {
 		var $el = this.$contact;
-		if ($el.is(':visible')) { //Hide
+		//show or hide (test visibility)
+		if (x === undefined) {
+			$('#contact_button').toggleClass('active');
+			if ($el.is(':visible')) { //Hide
+				$el.animate({'opacity': 0}, 1000, function() {
+					$el.css('display', 'none');
+					window.history.pushState({page: 'home'}, 'Raphael Cohen', '/');
+				});
+			} else { //Show
+				$el.css('display', 'block');
+				window.history.pushState({page: 'contact'}, 'Contact | Raphael Cohen', 'contact');
+				$el.animate({'opacity': 1}, 1000);
+			}
+		} 
+
+		//hide only
+		else {
+			$('#contact_button').removeClass('active');
 			$el.animate({'opacity': 0}, 1000, function() {
 				$el.css('display', 'none');
-				window.history.replaceState({page: 'home'}, 'Raphael Cohen', '/');
+				window.history.pushState({page: 'home'}, 'Raphael Cohen', '/');
 			});
-		} else { //Show
-			$el.css('display', 'block');
-			window.history.replaceState({page: 'contact'}, 'Contact | Raphael Cohen', 'contact');
-			$el.animate({'opacity': 1}, 1000);
 		}
 	},
 
 	popHandler: function popHandler(event) {
 		if (event.state.page === 'home') {
 			event.preventDefault();
-			controller.toggleContact();
+			controller.toggleContact(1);
 		}
 	},
 
